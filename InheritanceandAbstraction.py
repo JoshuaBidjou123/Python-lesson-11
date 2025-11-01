7a8a3ad326f402fcf78d27b8b818a107f51702e7
JoshuaBidjou123 <kristen.bidjou@gmail.com>
2025-11-01 12:26:24 -0400

    Commit

---
+++ InheritanceandAbstraction.py
@@ -0,0 +1,25 @@
+# parent class
+class person ( object ):
+    # __init__ is known as the constructor
+    def __init__(self, name, idnumber):
+        self.name = name
+        self.idnumber = idnumber
+
+    def display(self):
+        print(self.name)
+        print(self.idnumber)
+
+# child class
+class Employee(person):
+     def __init__(self, name, idnumber, salary, post):
+         self.salary = salary
+         self.post = post
+
+         # invoking the __init__ of the parent class
+         person.__init__(self, name, idnumber) 
+
+# creation of an object variable or instance
+a = Employee('Penguin', 20210401, 1500, "Intern")
+
+# calling the function of the class Person using its instance
+a.display()
